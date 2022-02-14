25a496e932607a2fe41a4f8e3a9f9812ded80b27
darwageddon <canberk.ertrn@hotmail.com>
2022-02-14 23:48:27 +0300

    turnSpeed variable
    turnSpeed variable was added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed; 
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         // We'll move our vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
 
 
     }   
